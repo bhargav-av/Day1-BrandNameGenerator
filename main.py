77f319e519cc7d3350be3cf8612c3aec7810e1bb
Bhargav AV <venkatabhargavit@gmail.com>
2024-01-18 21:12:25 +0530

    This is a Brand Name Generator program

---
+++ main.py
@@ -0,0 +1,14 @@
+# Create a greeting for your program.
+print("Welcome to Brand Name Generator")
+
+# Ask the user for the city that they grew up in.
+city_name = input("Enter the name of the city your grew up : \n")
+
+# Ask the user for the name of a pet.
+pet_name = input("Enter the name of your pet : \n")
+
+# Combine the name of their city and pet and show them their band name.
+print(f"Your Brand could be: {city_name + pet_name}")
+
+
+
